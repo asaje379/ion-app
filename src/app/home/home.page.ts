da297b992ac31bd6556f63501bb6bb66c5da95a1
asaje379 <salemaffa@gmail.com>
2020-07-14 13:12:16 +0100

    sec

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { createAnimation } from "@ionic/core";
+import { DbService } from '../service/db.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  data: any[] = [];
+
+  constructor(
+    private service: DbService
+  ) { }
+
+  ngOnInit(): void {
+    this.service.getData().subscribe((res: any[]) => {
+      this.data = res;
+    });
+    this.animate();
+  }
+
+  async animate() {
+    const animationA = createAnimation()
+      .addElement(document.querySelector('#logo'))
+      .duration(1500)
+      .keyframes([
+        { offset: 0, transform: 'rotate(-360deg) scale(0)' },
+        { offset: 0.5, transform: 'rotate(-180deg) scale(1.5)' },
+        { offset: 1, transform: 'rotate(0deg) scale(1)' }
+      ]);
+    const animationB = createAnimation()
+      .addElement(document.querySelector('.img-ctn'))
+      .delay(1500)
+      .duration(500)
+      .keyframes([
+        { offset: 0, height: '90vh' },
+        { offset: 1, height: '52vh' },
+      ]);
+    animationA.play();
+    animationB.play();
+  }
 
 }
